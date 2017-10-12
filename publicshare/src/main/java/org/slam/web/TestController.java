ccdd852809103f5f0853cfacd84707f762b909c2
bit <bit@bitcamp>
2017-10-12 17:26:56 +0900

    test......

--- publicshare/src/main/java/org/slam/web/TestController.java
+++ publicshare/src/main/java/org/slam/web/TestController.java
@@ -2,6 +2,7 @@ package org.slam.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +12,10 @@ public class TestController {
 	@GetMapping("/test2")
 	public void Test2() {
 		System.out.println("¾È³ç¾È³ç");
+	}
+	
+	@PostMapping("/test2")
+	public void test2Post() {
+		
 	}
 }
