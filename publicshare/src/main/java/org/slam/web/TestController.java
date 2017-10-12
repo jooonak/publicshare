f070e32aa8f436da5441075a67d6cfa8d042ae92
bit <bit@bitcamp>
2017-10-12 17:23:22 +0900

    Test2 commit

---
+++ publicshare/src/main/java/org/slam/web/TestController.java
@@ -0,0 +1,15 @@
+package org.slam.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test/*")
+public class TestController {
+
+	@GetMapping("/test2")
+	public void Test2() {
+		System.out.println("¾È³ç¾È³ç");
+	}
+}
