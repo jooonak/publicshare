b2df3356141032c0d7db53969b8296b2d0e257a3
soobok <bit@bitcamp>
2017-10-12 18:28:52 +0900

    test controller 제작

--- publicshare/src/main/java/org/slam/web/TestController.java
+++ publicshare/src/main/java/org/slam/web/TestController.java
@@ -7,9 +7,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/test/*")
 public class TestController {
-
-	@GetMapping("/main")
-	public void goMain() {
-		System.out.println("hongbean test");
+	
+	//나의 물품 관리 탭
+	@GetMapping("/itemmanage")
+	public void myItems() {	
 	}
+	
+	//대출한 물품 관리 
+	@GetMapping("/return")
+	public void myReturns() {	
+	}
+	
+	//대여 목록 화면(메인)
+	@GetMapping("/main")
+	public void goMain() {	
+	}
+		
+	@GetMapping("/view")
+	public void view() {	
+	}	
+	
+	@GetMapping("/login")
+	public void login() {	
+	}	
 }
