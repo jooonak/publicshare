9657702d5484ebc2e7d91565ac093e6bb0ccea04
joonhak <bit@bitcamp>
2017-10-27 15:21:09 +0900

    1. merge

--- publicshare/src/main/java/org/slam/service/ReturnServiceImpl.java
+++ publicshare/src/main/java/org/slam/service/ReturnServiceImpl.java
@@ -15,71 +15,76 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class ReturnServiceImpl implements ReturnService{
 
-	@Inject
-	ReturnMapper returnMapper;
-	
-	@Inject
-	ResBookMapper resBookMapper;
+    @Inject
+    ReturnMapper returnMapper;
+    
+    @Inject
+    ResBookMapper resBookMapper;
+    
+    @Override
+    public Criteria setCri(Criteria cri, String mid) {
+        cri.setTotal(returnMapper.getTotal(mid));
+        return cri;
+    }
 
-	@Transactional
-	@Override
-	public void request(ReservationDTO dto) {
-		if(dto.getStatus().equals("cancel")) {
-			resBookMapper.updateResCntDown(dto.getBno());
-		}
-		System.out.println(dto);
-		returnMapper.request(dto);
-	}
+    @Transactional
+    @Override
+    public void request(ReservationDTO dto) {
+        if(dto.getStatus().equals("cancel")) {
+            resBookMapper.updateResCntDown(dto.getBno());
+        }
+        System.out.println(dto);
+        returnMapper.request(dto);
+    }
 
-	@Override
-	public List<Map<String, Object>> checkItem(String mid) {
-		return returnMapper.checkItem(mid);
-	}
-	
-	@Transactional
-	@Override
-	public void returnConfirm(int bno, int rno) {
-		returnMapper.returnConfirm(bno, rno);
-		resBookMapper.updateResCntDown(bno);
-	}
+    @Override
+    public List<Map<String, Object>> checkItem(String mid) {
+        return returnMapper.checkItem(mid);
+    }
+    
+    @Transactional
+    @Override
+    public void returnConfirm(int bno, int rno) {
+        returnMapper.returnConfirm(bno, rno);
+        resBookMapper.updateResCntDown(bno);
+    }
 
-	@Override
-	public void returnReject(int rno) {
-		returnMapper.returnReject(rno);
-	}
+    @Override
+    public void returnReject(int rno) {
+        returnMapper.returnReject(rno);
+    }
 
-	@Override
-	public void acceptBookReserve(ReservationDTO dto) {
-		resBookMapper.confirmUpdateApplyData(dto.getRno());
-	}
-	
-	@Transactional
-	@Override
-	public void refuseBookReserve(ReservationDTO dto) {
-		resBookMapper.rejectUpdateApplyData(dto);
-		resBookMapper.updateResCntDown(dto.getBno());
-	}
+    @Override
+    public void acceptBookReserve(ReservationDTO dto) {
+        resBookMapper.confirmUpdateApplyData(dto.getRno());
+    }
+    
+    @Transactional
+    @Override
+    public void refuseBookReserve(ReservationDTO dto) {
+        resBookMapper.rejectUpdateApplyData(dto);
+        resBookMapper.updateResCntDown(dto.getBno());
+    }
 
-	@Override
-	public List<Map<String, Object>> checkReturn(String mid) {
-		return returnMapper.checkReturn(mid);
-	}
+    @Override
+    public List<Map<String, Object>> checkReturn(String mid) {
+        return returnMapper.checkReturn(mid);
+    }
 
-	@Override
-	public void checkReject(int rno) {
-		returnMapper.checkReject(rno);
-	}
+    @Override
+    public void checkReject(int rno) {
+        returnMapper.checkReject(rno);
+    }
 
-	@Override
-	public List<Map<String, Object>> getList(Criteria cri, String status, String mid) {
-		cri.setTotal(returnMapper.getTotal(mid, status));
-		return returnMapper.getList(cri, status, mid);
-	}
+    @Override
+    public List<Map<String, Object>> getList(int page, String status, String mid) {
+        int skip = (page - 1) * 8;
+        return returnMapper.getList(skip, status, mid);
+    }
 
-	@Override
-	public List<Map<String, Object>> loanHistoryList(String mid) {
-		
-		return returnMapper.loanHistoryList(mid);
-	}
-
+    @Override
+    public List<Map<String, Object>> loanHistoryList(String mid) {
+        
+        return returnMapper.loanHistoryList(mid);
+    }
 }
--- publicshare/src/main/java/org/slam/web/ReturnRestController.java
+++ publicshare/src/main/java/org/slam/web/ReturnRestController.java
@@ -26,73 +26,73 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 
 public class ReturnRestController {
 
-	@Inject
-	ReturnService service;
-	
-	@PostMapping("/request")
-	public void returnRequest(@RequestBody ReservationDTO dto) {
-		//반납 요청하는 메서드 -JH
-		System.out.println(dto);
-		if(dto.getStatus().equals("onapply") || dto.getStatus().equals("onres")) {
-			// 예약, 대여신청의 취소
-			dto.setStatus("cancel");
-		} else {
-			// 반납 신청
-			dto.setStatus("onreturn");
-		}
-		service.request(dto);
-	}
+    @Inject
+    ReturnService service;
+    
+    @PostMapping("/request")
+    public void returnRequest(@RequestBody ReservationDTO dto) {
+        //반납 요청하는 메서드 -JH
+        System.out.println(dto);
+        if(dto.getStatus().equals("onapply") || dto.getStatus().equals("onres")) {
+            // 예약, 대여신청의 취소
+            dto.setStatus("cancel");
+        } else {
+            // 반납 신청
+            dto.setStatus("onreturn");
+        }
+        service.request(dto);
+    }
 
-	@GetMapping("/check")
-	public List<Map<String, Object>> checkOnReturn(@SessionAttribute(value="member", required=false) MemberDTO dto){
-		//내 소유의 책에 대해 반납 신청이 있는지 체크하고, 있을 때 그 책의 정보를 가져오는 메서드 -JH 
-		return service.checkItem(dto.getMid());
-	}
+    @GetMapping("/check")
+    public List<Map<String, Object>> checkOnReturn(@SessionAttribute(value="member", required=false) MemberDTO dto){
+        //내 소유의 책에 대해 반납 신청이 있는지 체크하고, 있을 때 그 책의 정보를 가져오는 메서드 -JH 
+        return service.checkItem(dto.getMid());
+    }
 
-	@PostMapping("/returnconfirm")
-	public void returnConfirm(@RequestBody ReservationDTO dto) {
-		//반납 신청이 있을 때 반납 처리 하는 메서드 -JH
-		service.returnConfirm(dto.getBno(), dto.getRno());
-	}
-	
-	@PostMapping("/returnreject")
-	public void returnReject(@RequestBody ReservationDTO dto) {
-		//반납 신청이 있을 때 반납 거부 처리 하는 메서드 -JH
-		service.returnReject(dto.getRno());
-	}
-	
-	@PostMapping("/reserveconfirm")
-	public void confirm(@RequestBody ReservationDTO dto) {
-		service.acceptBookReserve(dto);
-	}
-	
-	
-	@PostMapping("/reservereject")
-	public void reject(@RequestBody ReservationDTO dto) {
-		service.refuseBookReserve(dto);
-	}
-	
-	@GetMapping("/checkReturn")
-	public List<Map<String, Object>> checkReturn(@SessionAttribute(value="member", required=false) MemberDTO dto){
-		//내가 반납 신청 했으나, 상대방에 의해 거절된 정보를 가져오는 메서드 -JH
-		return service.checkReturn(dto.getMid());
-	}
-	
-	@PostMapping("/checkreject")
-	public void checkReject(@RequestBody ReservationDTO dto) {
-		service.checkReject(dto.getRno());
-	}
-	
-	@GetMapping("/list/{status}/{page}")
-	public List<Map<String, Object>> List(@PathVariable("page") Criteria cri, @PathVariable("status") String status,
-			@SessionAttribute(value = "member", required = false) MemberDTO member){
-		return service.getList(cri, status, member.getMid());
-	}
-	
-	@GetMapping("/loanHistoryList")
-	public List<Map<String, Object>> loanHistoryList(@SessionAttribute(value = "member", required = false) MemberDTO dto) {
-		// 사용자의 과거 대여 이력(반납이 완료된 책에 대한 히스토리)_hb
-		return service.loanHistoryList(dto.getMid());
-	}
+    @PostMapping("/returnconfirm")
+    public void returnConfirm(@RequestBody ReservationDTO dto) {
+        //반납 신청이 있을 때 반납 처리 하는 메서드 -JH
+        service.returnConfirm(dto.getBno(), dto.getRno());
+    }
+    
+    @PostMapping("/returnreject")
+    public void returnReject(@RequestBody ReservationDTO dto) {
+        //반납 신청이 있을 때 반납 거부 처리 하는 메서드 -JH
+        service.returnReject(dto.getRno());
+    }
+    
+    @PostMapping("/reserveconfirm")
+    public void confirm(@RequestBody ReservationDTO dto) {
+        service.acceptBookReserve(dto);
+    }
+    
+    
+    @PostMapping("/reservereject")
+    public void reject(@RequestBody ReservationDTO dto) {
+        service.refuseBookReserve(dto);
+    }
+    
+    @GetMapping("/checkReturn")
+    public List<Map<String, Object>> checkReturn(@SessionAttribute(value="member", required=false) MemberDTO dto){
+        //내가 반납 신청 했으나, 상대방에 의해 거절된 정보를 가져오는 메서드 -JH
+        return service.checkReturn(dto.getMid());
+    }
+    
+    @PostMapping("/checkreject")
+    public void checkReject(@RequestBody ReservationDTO dto) {
+        service.checkReject(dto.getRno());
+    }
+    
+    @GetMapping("/list/{status}/{page}")
+    public List<Map<String, Object>> List(@PathVariable("page") int page, @PathVariable("status") String status,
+            @SessionAttribute(value = "member", required = false) MemberDTO member){
+        return service.getList(page, status, member.getMid());
+    }
+    
+    @GetMapping("/loanHistoryList")
+    public List<Map<String, Object>> loanHistoryList(@SessionAttribute(value = "member", required = false) MemberDTO dto) {
+        // 사용자의 과거 대여 이력(반납이 완료된 책에 대한 히스토리)_hb
+        return service.loanHistoryList(dto.getMid());
+    }
 
 }
